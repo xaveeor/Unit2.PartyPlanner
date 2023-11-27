28020afd504873e11f35390ff5e8092385905ff3
xavier <xaviergomez@live.com>
2023-11-27 15:04:11 -0700

    finished

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const BASE_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-ACC-ET-WEB-PT-A/events`;
 const mainEl = document.querySelector('main');
 const formEl = document.querySelector('form');
-const partyName = document.querySelector('#partyname');
+const partyName = document.querySelector('#partyName');
+const locations = document.querySelector('#locations');
+const date = document.querySelector('#date');
+const description = document.querySelector('#description');
+
 
 async function getPartys() {
-
     const response = await fetch(BASE_URL);
     const data = await response.json();
-    console.log(data.data);
-    return data.data;}
+    console.log(data.data)
+    return data.data;
+    
+  }
  
 function render(partys) {
   const template = partys.map(party => {
@@ -24,14 +29,16 @@ function render(partys) {
     )
   }).join('');
   mainEl.innerHTML = template;
-}
+};
 
-async function PartyApp() {
+
+
+async function partyApp() {
   const partys = await getPartys();
   render(partys);
 }
+partyApp();
 
-PartyApp();
 
 formEl.addEventListener('submit', async (event) => {
   event.preventDefault();
@@ -41,28 +48,32 @@ formEl.addEventListener('submit', async (event) => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        name: partyname.value,
-        location: location.value,
-        date: date.value,
+        name: partyName.value,
         description: description.value,
+        date: date.value,
+        location: locations.value,
       })
     });
-  
-    partyname.value = '';
+    partyName.value = '';
     date.value = '';
     description.value = '';
     locations.value = '';
+    
   
-    PartyApp();
+    partyApp();
+ 
+
 });
+
+
+
 
 mainEl.addEventListener('click', async (event) => {
   if(event.target.matches('button')) {
     const id = event.target.dataset.id;
-    await fetch(`${BASE_URL}`, {
+    await fetch(`${BASE_URL}/${id}`, {
       method: 'DELETE',
     });
-    PartyApp();
+    partyApp();
   }
-}); 
-
+});
